9d65f8431e18665f347fa8afcf6a8584d5c43186
Curran Heppenstall <curranhepp@gmail.com>
2013-09-12 11:35:16 -0700

    changing the name of the lang directory to languages and all references to it

--- class-dash-debug.php
+++ class-dash-debug.php
@@ -143,7 +143,7 @@ class DashDebug {
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( dirname( __FILE__ ) ) . '/lang/' );
+		load_plugin_textdomain( $domain, FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
